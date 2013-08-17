1c6754113cf08648df88e422509f8c4d3954e28b
manji_black <manji.black@gmail.com>
2013-08-18 00:06:07 +0900

    コンピュータ側がとりあえず動くようになったので、一旦コミット。 ＜残課題＞ ・プレイヤー駒がクリックできない時がある。 ・コンピュータの手の探索において、駒を取る動作が一時的な領域ではない、グローバルな領域を使っている。そのため、角を２つ取る、等のケースが発生する。

--- js/shogi.js
+++ js/shogi.js
@@ -6,10 +6,11 @@
 /*****************************/
 /*	ãÓÇÃÉNÉâÉX               */
 /*****************************/
-function Piece(kind, owner, promoted) {
+function Piece(kind, owner, promoted, point) {
 	this.kind = kind;
 	this.owner = owner;
 	this.promoted = promoted;
+	this.point = point;
 	// à⁄ìÆâ¬î\Ç»èÍèäÅB
 	this.area = new Array();
 
@@ -19,7 +20,7 @@ function Piece(kind, owner, promoted) {
 			this.area.push(array[i]);
 		}
 		// alert(this.area[0][0]);
-	}
+	};
 	
 };
 
@@ -38,8 +39,54 @@ function Board() {
 	this.map = new Array(9);
 	for (var i=0;i<9;i++) {
 		this.map[i]=new Array(9);
-	}	
-}
+	}
+	
+	this.pieceNum = {};
+	this.pieceNum['PLAYER'] = 0;
+	this.pieceNum['OPPONENT'] = 0;
+	
+};
+
+/** è´ä˚î’ÇÃÉNÉçÅ[ÉìÇÃçÏê¨ */
+Board.prototype.clone = function() {
+	var b = new Board();
+	for (var i=0; i<9; i++) {
+		for (var j=0; j<9; j++) {
+			b.map[i][j] = board.map[i][j];
+		};
+	};
+	b.pieceNum['PLAYER'] = board.pieceNum['PLAYER'];
+	b.pieceNum['OPPONENT'] = board.pieceNum['OPPONENT'];
+	
+	return b;
+}; 
+
+/** åªç›ÇÃã«ñ ÇÃï]âø */
+Board.prototype.eval = function(turn) {
+	var playerPoint, opponentPoint;
+	
+	playerPoint = 0;
+	opponentPoint = 0;
+	
+	for (var i=0; i<9; i++) {
+		for (var j=0; j<9; j++) {
+			var p = this.map[i][j];
+			if (p.owner == PLAYER) {
+				playerPoint += p.point;
+			} else if (p.owner == OPPONENT) {
+				opponentPoint += p.point;
+			};
+		};
+	};
+		
+	if (turn == PLAYER) {
+		opponentPoint *= -1;
+	} else if (turn == OPPONENT) {
+		playerPoint *= -1;
+	};
+	
+	return playerPoint + opponentPoint;
+};
 
 /************************************************************/
 // Constant Value
@@ -83,7 +130,7 @@ const PLAYER_AREA_Y0 = 183;
 const X_SIZE = 43;
 const Y_SIZE = 48;
 
-const BLANK_PIECE = new Piece("Åõ", BLANK);
+const BLANK_PIECE = new Piece("Åõ", BLANK, false, 0);
 
 const OPP_FU_AREA = new Array([1, 0]);
 const OPP_KYO_AREA = 
@@ -153,6 +200,23 @@ const MY_UMA_AREA =
 const MY_OU_AREA = 
 		new Array([-1, 0], [-1, -1], [-1, 1], [0, -1], [0, 1], [1, 0], [1, 1], [1, -1]);
 
+const FU_POINT = 1;
+const KYO_POINT = 10;
+const KEI_POINT = 20;
+const GIN_POINT = 30;
+const KIN_POINT = 80;
+const KAKU_POINT = 300;
+const HISHA_POINT = 400;
+const OU_POINT = 900;
+
+const TO_POINT = 40;
+const NARIKYO_POINT = 50;
+const NARIKEI_POINT = 60;
+const NARIGIN_POINT = 70;
+const UMA_POINT = 500;
+const RYU_POINT = 600;
+
+
 /************************************************************/
 // Variables
 /************************************************************/
@@ -177,6 +241,7 @@ pieceInHand[OPPONENT] = new Array();
 var currentTurn = PLAYER;
 var winner = BLANK;
 
+
 /************************************************************/
 /* Operation code                                           */
 /************************************************************/
@@ -200,6 +265,9 @@ window.addEventListener
 	 false);
 
 initGame();
+if (currentTurn == OPPONENT) {
+	makeOpponentMove();
+}
 
 
 /************************************************************/
@@ -235,66 +303,68 @@ function initMap()
 	var i, j;
 	
 	// àÍçsñ⁄
-	board.map[0][0] = new Piece("çÅé‘", OPPONENT, false);
+	board.map[0][0] = new Piece("çÅé‘", OPPONENT, false, KYO_POINT);
 	board.map[0][0].setArea(OPP_KYO_AREA);
 	
-	board.map[0][1] = new Piece("åjîn", OPPONENT, false);
+	board.map[0][1] = new Piece("åjîn", OPPONENT, false, KEI_POINT);
 	board.map[0][1].setArea(OPP_KEI_AREA);
 
-	board.map[0][2] = new Piece("ã‚", OPPONENT, false);
+	board.map[0][2] = new Piece("ã‚", OPPONENT, false, GIN_POINT);
 	board.map[0][2].setArea(OPP_GIN_AREA);
 
-	board.map[0][3] = new Piece("ã‡", OPPONENT, false);
+	board.map[0][3] = new Piece("ã‡", OPPONENT, false, KIN_POINT);
 	board.map[0][3].setArea(OPP_KIN_AREA);
 
-	board.map[0][4] = new Piece("â§", OPPONENT, false);
+	board.map[0][4] = new Piece("â§", OPPONENT, false, OU_POINT);
 	board.map[0][4].setArea(OPP_OU_AREA);
 
-	board.map[0][5] = new Piece("ã‡", OPPONENT, false);
+	board.map[0][5] = new Piece("ã‡", OPPONENT, false, KIN_POINT);
 	board.map[0][5].setArea(OPP_KIN_AREA);
 
-	board.map[0][6] = new Piece("ã‚", OPPONENT, false);
+	board.map[0][6] = new Piece("ã‚", OPPONENT, false, GIN_POINT);
 	board.map[0][6].setArea(OPP_GIN_AREA);
 
-	board.map[0][7] = new Piece("åjîn", OPPONENT, false);
+	board.map[0][7] = new Piece("åjîn", OPPONENT, false, KEI_POINT);
 	board.map[0][7].setArea(OPP_KEI_AREA);
 
-	board.map[0][8] = new Piece("çÅé‘", OPPONENT, false);
+	board.map[0][8] = new Piece("çÅé‘", OPPONENT, false, KYO_POINT);
 	board.map[0][8].setArea(OPP_KYO_AREA);
 
 	// ìÒçsñ⁄
 	board.map[1][0] = BLANK_PIECE;
 
-	board.map[1][1] = new Piece("îÚé‘", OPPONENT, false);
+	board.map[1][1] = new Piece("îÚé‘", OPPONENT, false, HISHA_POINT);
 	board.map[1][1].setArea(OPP_HISHA_AREA);
 	
 	for (j=2;j<7;j++) {
 		board.map[1][j] = BLANK_PIECE;
 	}
 	
-	board.map[1][7] = new Piece("äp", OPPONENT, false);
+	board.map[1][7] = new Piece("äp", OPPONENT, false, KAKU_POINT);
 	board.map[1][7].setArea(OPP_KAKU_AREA);
 	
 	board.map[1][8] = BLANK_PIECE;
 
 	// éOçsñ⁄
 	for (j=0;j<9;j++) {
-		board.map[2][j] = new Piece("ï‡", OPPONENT, false);
+		board.map[2][j] = new Piece("ï‡", OPPONENT, false, FU_POINT);
 		board.map[2][j].setArea(OPP_FU_AREA);
 		// alert("["+0+"]["+j+"]="+ map[0][j].kind);
 	}
+	
+	board.opponentPieceNum = 20;
 	
 	// élÅ`òZçsñ⁄
 	for (i=3;i<6;i++) { 
 		for (j=0;j<9;j++) {
 			board.map[i][j] = BLANK_PIECE;
 			// alert("["+i+"]["+j+"]="+map[i][j].kind);
-		}
+		};
 	}
 	
 	// éµçsñ⁄
 	for (j=0;j<9;j++) {
-		board.map[6][j] = new Piece("ï‡", PLAYER, false);
+		board.map[6][j] = new Piece("ï‡", PLAYER, false, FU_POINT);
 		board.map[6][j].setArea(MY_FU_AREA);
 		// alert("["+8+"]["+j+"]="+map[8][j].kind);
 	}
@@ -310,46 +380,49 @@ function initMap()
 	// î™çsñ⁄
 	board.map[7][0] = BLANK_PIECE;
 
-	board.map[7][1] = new Piece("äp", PLAYER, false);
+	board.map[7][1] = new Piece("äp", PLAYER, false, KAKU_POINT);
 	board.map[7][1].setArea(MY_KAKU_AREA);
 	
 	for (j=2;j<7;j++) {
 		board.map[1][j] = BLANK_PIECE;
 	}
 	
-	board.map[7][7] = new Piece("îÚé‘", PLAYER, false);
+	board.map[7][7] = new Piece("îÚé‘", PLAYER, false, HISHA_POINT);
 	board.map[7][7].setArea(MY_HISHA_AREA);
 	
 	board.map[7][8] = BLANK_PIECE;
 
 	// ã„çsñ⁄
-	board.map[8][0] = new Piece("çÅé‘", PLAYER, false);
+	board.map[8][0] = new Piece("çÅé‘", PLAYER, false, KYO_POINT);
 	board.map[8][0].setArea(MY_KYO_AREA);
 	
-	board.map[8][1] = new Piece("åjîn", PLAYER, false);
+	board.map[8][1] = new Piece("åjîn", PLAYER, false, KEI_POINT);
 	board.map[8][1].setArea(MY_KEI_AREA);
 
-	board.map[8][2] = new Piece("ã‚", PLAYER, false);
+	board.map[8][2] = new Piece("ã‚", PLAYER, false, GIN_POINT);
 	board.map[8][2].setArea(MY_GIN_AREA);
 
-	board.map[8][3] = new Piece("ã‡", PLAYER, false);
+	board.map[8][3] = new Piece("ã‡", PLAYER, false, KIN_POINT);
 	board.map[8][3].setArea(MY_KIN_AREA);
 
-	board.map[8][4] = new Piece("â§", PLAYER, false);
+	board.map[8][4] = new Piece("â§", PLAYER, false, OU_POINT);
 	board.map[8][4].setArea(MY_OU_AREA);
 
-	board.map[8][5] = new Piece("ã‡", PLAYER, false);
+	board.map[8][5] = new Piece("ã‡", PLAYER, false, KIN_POINT);
 	board.map[8][5].setArea(MY_KIN_AREA);
 
-	board.map[8][6] = new Piece("ã‚", PLAYER, false);
+	board.map[8][6] = new Piece("ã‚", PLAYER, false, GIN_POINT);
 	board.map[8][6].setArea(MY_GIN_AREA);
 
-	board.map[8][7] = new Piece("åjîn", PLAYER, false);
+	board.map[8][7] = new Piece("åjîn", PLAYER, false, KEI_POINT);
 	board.map[8][7].setArea(MY_KEI_AREA);
 
-	board.map[8][8] = new Piece("çÅé‘", PLAYER, false);
+	board.map[8][8] = new Piece("çÅé‘", PLAYER, false, KYO_POINT);
 	board.map[8][8].setArea(MY_KYO_AREA);
 	
+	board.pieceNum['PLAYER'] = 20;
+	board.pieceNum['OPPONENT'] = 20;
+
 	pieceInHand[PLAYER].length = 0;
 	pieceInHand[OPPONENT].length = 0;
 
@@ -563,7 +636,7 @@ function onBoardAction()
 		var selectedPiece = board.map[selectedCell.row][selectedCell.column];
 		if (selectedPiece.owner == currentTurn) {
 			if (isMovable(selectedPiece, selectedCell, clickedCell)) {
-				movePiece(clickedCell.row, clickedCell.column);
+				board = movePiece(board, selectedCell, clickedCell);
 				printMap();
 				changeTurn();
 			} else {
@@ -599,6 +672,13 @@ function isMovable(piece, src, dest) {
 
 	// alert("Src: (" + src.row + ", " + src.column + "), " + 
 	//		"Dest: (" + dest.row + ", " + dest.column + ") ");
+	
+	// à⁄ìÆêÊÇ™è´ä˚î’ÇÃîÕàÕäOÇ»ÇÁÅAà⁄ìÆïsâ¬
+	if (((dest.row < 0) || (8 < dest.row)) || 
+		((dest.column < 0) || (8 < dest.column))) {
+		return false;
+	}
+	
 	for (i=0;i<piece.area.length;i++) {
 		// à⁄ìÆêÊÇÃÉ}ÉXÇ™ÅAÇªÇÃãÓÇÃà⁄ìÆîÕàÕì‡
 		if ((src.row + piece.area[i][0] == dest.row) &&
@@ -677,10 +757,10 @@ function selectPiece(row, column)
 /*****************************/
 /* ãÓÇÃà⁄ìÆ                  */
 /*****************************/
-function movePiece(row, column)
+function movePiece(brd, src, dst)
 {
 	// à⁄ìÆêÊÇ…ãÓÇ™Ç†ÇÈÇ»ÇÁÅAãÓÇéÊÇÈ
-	var targetPiece = board.map[row][column];
+	var targetPiece = brd.map[dst.row][dst.column];
 	if ((targetPiece.owner != currentTurn) && (targetPiece.owner != BLANK)) {
 		// â§ÇéÊÇ¡ÇΩÇÁèüÇø
 		if (targetPiece.kind == "â§") {
@@ -690,28 +770,36 @@ function movePiece(row, column)
 		}
 		targetPiece.owner = currentTurn;
 		pieceInHand[currentTurn].push(targetPiece);
-		board.map[row][column] = BLANK_PIECE;
+		brd.map[dst.row][dst.column] = BLANK_PIECE;
+		
+		if (currentTurn == PLAYER) {
+			brd.pieceNum['PLAYER'] -= 1;
+		} else if (currentTurn == OPPONENT) {
+			brd.pieceNum['OPPONENT'] -= 1;
+		}
 	}
 	
 	// ãÓÇÃà⁄ìÆ
-	var selectedPiece = board.map[selectedCell.row][selectedCell.column];
+	var selectedPiece = brd.map[src.row][src.column];
 	// ëäéËêwínÇ…ì¸Ç¡ÇΩÇÁê¨ÇÈ
-	if (((currentTurn == PLAYER) && (row <= 2)) || 
-	    ((currentTurn == OPPONENT) && (row >= 6))) {
+	if (((currentTurn == PLAYER) && (dst.row <= 2)) || 
+	    ((currentTurn == OPPONENT) && (dst.row >= 6))) {
 	    if (selectedPiece.promoted == false) {
 			selectedPiece = promotePiece(selectedPiece)
-	    }
+	    };
 	}
-	board.map[row][column] = selectedPiece;
+	brd.map[dst.row][dst.column] = selectedPiece;
 
 	// à⁄ìÆëOÇÃãÓëIëÇÃâèú
-	board.map[selectedCell.row][selectedCell.column] =BLANK_PIECE;
+	brd.map[src.row][src.column] =BLANK_PIECE;
 	document.getElementById("selected_cell").innerText
 		= "Selected: ";
 	selectState = UNSELECTED;
 	document.
-		getElementById("cell"+selectedCell.row+"-"+selectedCell.column).
+		getElementById("cell"+src.row+"-"+src.column).
 			style.backgroundImage = '';
+	
+	return brd;
 }
 
 /********************************/
@@ -722,44 +810,44 @@ function promotePiece(piece)
 	var promPiece = piece;
 	if (PLAYER == piece.owner) {
 		if ("ï‡" == piece.kind) {
-			promPiece = new Piece("Ç∆", PLAYER, true);
+			promPiece = new Piece("Ç∆", PLAYER, true, TO_POINT);
 			promPiece.setArea(MY_KIN_AREA);
 		} else if ("çÅé‘" == piece.kind) {
-			promPiece = new Piece("ê¨çÅ", PLAYER, true);
+			promPiece = new Piece("ê¨çÅ", PLAYER, true, NARIKYO_POINT);
 			promPiece.setArea(MY_KIN_AREA);
 		} else if ("åjîn" == piece.kind) {
-			promPiece = new Piece("ê¨åj", PLAYER, true);
+			promPiece = new Piece("ê¨åj", PLAYER, true, NARIKEI_POINT);
 			promPiece.setArea(MY_KIN_AREA);
 		} else if ("ã‚" == piece.kind) {
-			promPiece = new Piece("ê¨ã‚", PLAYER, true);
+			promPiece = new Piece("ê¨ã‚", PLAYER, true, NARIGIN_POINT);
 			promPiece.setArea(MY_KIN_AREA);
 		} else if ("îÚé‘" == piece.kind) {
-			promPiece = new Piece("ó¥", PLAYER, true);
+			promPiece = new Piece("ó¥", PLAYER, true, RYU_POINT);
 			promPiece.setArea(MY_RYU_AREA);
 		} else if ("äp" == piece.kind) {
-			promPiece = new Piece("în", PLAYER, true);
+			promPiece = new Piece("în", PLAYER, true, UMA_POINT);
 			promPiece.setArea(MY_UMA_AREA);
-		} 
+		};
 	} else if (OPPONENT == piece.owner) {
 		if ("ï‡" == piece.kind) {
-			promPiece = new Piece("Ç∆", OPPONENT, true);
+			promPiece = new Piece("Ç∆", OPPONENT, true, TO_POINT);
 			promPiece.setArea(OPP_KIN_AREA);
 		} else if ("çÅé‘" == piece.kind) {
-			promPiece = new Piece("ê¨çÅ", OPPONENT, true);
+			promPiece = new Piece("ê¨çÅ", OPPONENT, true, NARIKYO_POINT);
 			promPiece.setArea(OPP_KIN_AREA);
 		} else if ("åjîn" == piece.kind) {
-			promPiece = new Piece("ê¨åj", OPPONENT, true);
+			promPiece = new Piece("ê¨åj", OPPONENT, true, NARIKEI_POINT);
 			promPiece.setArea(OPP_KIN_AREA);
 		} else if ("ã‚" == piece.kind) {
-			promPiece = new Piece("ê¨ã‚", OPPONENT, true);
+			promPiece = new Piece("ê¨ã‚", OPPONENT, true, NARIGIN_POINT);
 			promPiece.setArea(OPP_KIN_AREA);
 		} else if ("îÚé‘" == piece.kind) {
-			promPiece = new Piece("ó¥", OPPONENT, true);
+			promPiece = new Piece("ó¥", OPPONENT, true, RYU_POINT);
 			promPiece.setArea(OPP_RYU_AREA);
 		} else if ("äp" == piece.kind) {
-			promPiece = new Piece("în", OPPONENT, true);
+			promPiece = new Piece("în", OPPONENT, true, KAKU_POINT);
 			promPiece.setArea(OPP_UMA_AREA);
-		} 
+		}; 
 	}
 	return promPiece;
 }
@@ -772,42 +860,42 @@ function demotePiece(piece)
 	var demPiece = piece;
 	if (PLAYER == piece.owner) {
 		if ("Ç∆" == piece.kind) {
-			demPiece = new Piece("ï‡", PLAYER, false);
+			demPiece = new Piece("ï‡", PLAYER, false, FU_POINT);
 			demPiece.setArea(MY_FU_AREA);
 		} else if ("ê¨çÅ" == piece.kind) {
-			demPiece = new Piece("çÅé‘", PLAYER, false);
+			demPiece = new Piece("çÅé‘", PLAYER, false, KYO_POINT);
 			demPiece.setArea(MY_KYO_AREA);
 		} else if ("ê¨åj" == piece.kind) {
-			demPiece = new Piece("åjîn", PLAYER, false);
+			demPiece = new Piece("åjîn", PLAYER, false, KEI_POINT);
 			demPiece.setArea(MY_KEI_AREA);
 		} else if ("ê¨ã‚" == piece.kind) {
-			demPiece = new Piece("ã‚", PLAYER, false);
+			demPiece = new Piece("ã‚", PLAYER, false, GIN_POINT);
 			demPiece.setArea(MY_GIN_AREA);
 		} else if ("ó¥" == piece.kind) {
-			demPiece = new Piece("îÚé‘", PLAYER, false);
+			demPiece = new Piece("îÚé‘", PLAYER, false, HISHA_POINT);
 			demPiece.setArea(MY_HISHA_AREA);
 		} else if ("în" == piece.kind) {
-			demPiece = new Piece("äp", PLAYER, false);
+			demPiece = new Piece("äp", PLAYER, false, KAKU_POINT);
 			demPiece.setArea(MY_KAKU_AREA);
 		} 
 	} else if (OPPONENT == piece.owner) {
 		if ("Ç∆" == piece.kind) {
-			demPiece = new Piece("ï‡", OPPONENT, false);
+			demPiece = new Piece("ï‡", OPPONENT, false, FU_POINT);
 			demPiece.setArea(OPP_FU_AREA);
 		} else if ("ê¨çÅ" == piece.kind) {
-			demPiece = new Piece("çÅé‘", OPPONENT, false);
+			demPiece = new Piece("çÅé‘", OPPONENT, false, KYO_POINT);
 			demPiece.setArea(OPP_KYO_AREA);
 		} else if ("ê¨åj" == piece.kind) {
-			demPiece = new Piece("åjîn", OPPONENT, false);
+			demPiece = new Piece("åjîn", OPPONENT, false, KEI_POINT);
 			demPiece.setArea(OPP_KEI_AREA);
 		} else if ("ê¨ã‚" == piece.kind) {
-			demPiece = new Piece("ã‚", OPPONENT, false);
+			demPiece = new Piece("ã‚", OPPONENT, false, GIN_POINT);
 			demPiece.setArea(OPP_GIN_AREA);
 		} else if ("ó¥" == piece.kind) {
-			demPiece = new Piece("îÚé‘", OPPONENT, false);
+			demPiece = new Piece("îÚé‘", OPPONENT, false, HISHA_POINT);
 			demPiece.setArea(OPP_HISHA_AREA);
 		} else if ("în" == piece.kind) {
-			demPiece = new Piece("äp", OPPONENT, false);
+			demPiece = new Piece("äp", OPPONENT, false, KAKU_POINT);
 			demPiece.setArea(OPP_KAKU_AREA);
 		} 
 	}
@@ -889,21 +977,95 @@ function detectCellOnOppArea(hx, hy)
 		if (x < i+1) {
 			clickedOppAreaCell.column = i;
 			break;
-		}
+		};
 	}
 	y = (hy - OPP_AREA_Y0)/Y_SIZE;
 	for (i=0;i<5;i++) {
 		if (y < i+1) {
 			clickedOppAreaCell.row = i;
 			break;
-		}
-	}
+		};
+	};
+};
+
+/********************************/
+/* ëäéËÇÃéËî‘  */
+/********************************/
+function makeOpponentMove() {
+	board = getOpponentMove();
+	changeTurn();
 }
 
 /********************************/
-/* ëäéËÉNÉäÉbÉNéûÇÃÉAÉNÉVÉáÉì   */
+/* ÉRÉìÉsÉÖÅ[É^ë§ÇÃéËÇÃåàíË   */
 /********************************/
+function getOpponentMove() {
+	var i, j, k;
+	var nextBoard = board;	// éüÇÃã«ñ 
+	var piece;
+	var score = Number.NEGATIVE_INFINITY;
+	var max = Number.NEGATIVE_INFINITY;
+	
+	for (i=0; i<9; i++) {
+		for (j=0; j<9; j++) {
+			piece = board.map[i][j];
+			var src = new Cell(i, j);
+			if (piece.owner == OPPONENT) {
+				for (k=0; k < piece.area.length; k++) {
+					var r = i + piece.area[k][0];
+					var c = j + piece.area[k][1];
+					var dst = new Cell(r, c);
+					if (isMovable(piece, src, dst)) {
+						var brd = movePiece(board.clone(), src, dst);
+						score = negaAlpha(brd, 0, 
+										  Number.POSITIVE_INFINITY, 
+										  Number.NEGATIVE_INFINITY);
+						if (score > max) {
+							max = score;
+							nextBoard = brd;
+						};
+					};
+				};
+			};
+		};
+	};
+	
+	return nextBoard;
+};
 
+
+/*****************************/
+/* Nega-Éøñ@Ç≈ÇÃíTçı              */
+/*****************************/
+function negaAlpha(board, depth, a, b) {
+	var i, j, k;
+	var piece;
+	
+	if (depth == 0) {
+		return board.eval(currentTurn); 
+	}
+	
+	for (i=0; i<9; i++) {
+		for (j=0; j<9; j++) {
+			piece = board.map[i][j];
+			var src = new Cell(i, j);
+			if (piece.owner == OPPONENT) {
+				for (k=0; k < piece.area.length; k++) {
+					var dst = new Cell(i+piece.area[k][0], j+piece.area[k][1]);
+					if (isMovable(piece, src, dst)) {
+						var nextBoard = movePiece(board.clone(), src, dst);
+						a = Math.max(a, -negaAlpha(nextBoard, depth-1, -b, -a));
+						if (a > b) {
+							return a;
+						};
+					};
+				};
+			};
+		};
+	};
+	
+	return a;
+}
 
 
 /*****************************/
@@ -913,6 +1075,7 @@ function changeTurn()
 {
 	if (currentTurn == PLAYER) {
 		currentTurn = OPPONENT;
+		makeOpponentMove();
 	} else {
 		currentTurn = PLAYER;
 	}
@@ -984,5 +1147,5 @@ function hideDialog()
 	dialogTarget.style.visibility = "hidden";
 	
 	initGame();
-}
+};
 
