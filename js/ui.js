a0eb6ced62a2b5b2d7e10b821fda6d67d269a5db
manji_black <manji.black@gmail.com>
2013-09-05 00:37:17 +0900

    アプリ情報を追加。

--- js/ui.js
+++ js/ui.js
@@ -1,7 +1,19 @@
 $(function() {
+	/*=== 持ち駒エリアの表示 ===*/
 	$("#pieces_in_hand_area").hide();
 	
 	$("#pieces_in_hand_button").click(function() {
 		$("#pieces_in_hand_area").toggle("slide", 500);
+	});
+	
+	/*=== アプリ情報エリアの表示 ===*/
+	$("#app_info_area")
+		.accordion({header: "h5", 
+					collapsible: true});
+	
+	$("#app_info_area").hide();
+	
+	$("#app_info_button").click(function() {
+		$("#app_info_area").toggle("slide", 500);
 	});
 });
